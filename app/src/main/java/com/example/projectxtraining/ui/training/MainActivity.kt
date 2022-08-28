75720fd465fda1ac75f31984e112971fc8d18cca
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2022-08-28 22:22:09 +0300

    Signup fragment experiment.

--- app/src/main/java/com/example/projectxtraining/ui/training/MainActivity.kt
+++ app/src/main/java/com/example/projectxtraining/ui/training/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_ProjectXTraining)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
