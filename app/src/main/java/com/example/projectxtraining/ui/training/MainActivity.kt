67b393bad5a50856cfc5f7b867321e1d508b3b9a
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2022-08-28 22:35:49 +0300

    Signup fragment experiment. Fix.

--- app/src/main/java/com/example/projectxtraining/ui/training/MainActivity.kt
+++ app/src/main/java/com/example/projectxtraining/ui/training/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.projectxtraining.ui.training
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.projectxtraining.R
 import com.example.projectxtraining.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
